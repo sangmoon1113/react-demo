20c80aba78ebaacf77378fc4ba87fa02f15050d9
SANGMOON <sangmoon1113@gmail.com>
2023-10-16 22:04:47 +0900

    Initial commit

---
+++ src/main/groovy/com/example/reactdemo/ReactDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.reactdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReactDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ReactDemoApplication, args)
+    }
+
+}
